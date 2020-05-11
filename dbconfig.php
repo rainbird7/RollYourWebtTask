6e450cbff0e3e82032e4c845f3ef1e93a911e18b
Melih Tuerkalp <6161@htl.rennweg.at>
2020-05-11 20:04:31 +0200

    add database connection

--- dbconfig.php
+++ dbconfig.php
@@ -1,1 +1,20 @@
 <?php
+
+require('vendor/autoload.php');
+
+use Doctrine\DBAL\DriverManager;
+
+$connectionParams = array(
+    'dbname' => 'db',
+    'user' => 'root',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql'
+);
+
+
+try {
+    $conn = DriverManager::getConnection($connectionParams);
+} catch (\Exception $e) {
+    echo "what happened";
+}
